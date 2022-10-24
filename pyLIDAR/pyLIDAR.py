26e5e872fb2d85f4b4eccc584478a93761e4369c
Paradoxdruid <bonham@gmail.com>
2022-10-23 21:12:26 -0600

    fix: rewrite of data processing workflow

--- pyLIDAR/pyLIDAR.py
+++ pyLIDAR/pyLIDAR.py
@@ -1,41 +1,12 @@
 """Script to read LD06 LIDAR datastream and plot an updating graph of data."""
 
-import time
 from types import FrameType
-from typing import List, Optional, Tuple
+from typing import List, Optional
 
 import matplotlib.pyplot as plt
 import serial
 
 import read_data
-
-
-def convert_reading_to_polar(
-    reading: read_data.Reading,
-) -> Tuple[List[float], List[int]]:
-    """Convert a Reading object into a set of polar coordinates theta and r"""
-
-    num_points: int = len(reading.points)
-    start_angle: int = reading.start_angle
-    end_angle: int = reading.end_angle
-    step_size: int = int(((end_angle - start_angle) / num_points))
-    angles: List[int] = list(
-        range(
-            start_angle,
-            end_angle,
-            step_size,
-        )
-    )
-    angles = angles[:-1]
-
-    out_angles: List[float] = [
-        (each / 100) * 3.141529 / 180 for each in angles
-    ]  # convert to radians
-    # The division by 100 because angle is given as, i.e., 32457 for 324.57 degrees
-
-    distances: List[int] = [each.distance for each in reading.points]
-
-    return (out_angles, distances)
 
 
 def main(ser: serial.Serial) -> None:
@@ -59,28 +30,22 @@ def main(ser: serial.Serial) -> None:
 
         data: read_data.Reading = read_data.read_data(b"T" + raw_bytes)
 
-        # print(data)  # print to screen
-
         # ---------  DO YOUR DATA PROCESSING HERE --------------------
 
-        for each in data:
-            new_angles: List[float]
-            new_radii: List[int]
-            new_angles, new_radii = convert_reading_to_polar(each)
-            angles.extend(new_angles)
-            radii.extend(new_radii)
+        new_angles: List[float] = [
+            (point.angle / 100) * 3.141529 / 180 for point in data.points
+        ]  # convert to radians
+        new_radii: List[int] = [point.distance for point in data.points]
+        angles.extend(new_angles)
+        radii.extend(new_radii)
 
         # ---------  DO YOUR GRAPHING AND GRAPH UPDATES HERE ---------
 
-        # print("Plot")
-        # print(f"{angles=}")
-        # print(f"{radii=}")
         ax.plot(angles, radii)
         ax.grid(True)
         fig.canvas.draw()
         fig.show()
         plt.pause(0.05)  # Note this 50 ms delay to prevent freezes
-        time.sleep(1)
 
 
 # Code that runs when file is directly executed, i.e.: `python serial_LD.py`
--- pyLIDAR/read_data.py
+++ pyLIDAR/read_data.py
@@ -6,20 +6,7 @@ from typing import List
 SAMPLE = bytes.fromhex(
     (
         "54 2C 68 08 AB 7E E0 00 E4 DC 00 E2 D9 00 E5 D5 00 E3 D3 00 E4 D0 00 E9 CD 00"
-        "E4 CA 00 E2 C7 00 E9 C5 00 E5 C2 00 E5 C0 00 E5 BE 82 3A 1A 50 54 2C 68 08 AB"
-        "7E E0 00 E4 DC 00 E2 D9 00 E5 D5 00 E3 D3 00 E4 D0 00 E9 CD 00 E4 CA 00 E2 C7"
-        "00 E9 C5 00 E5 C2 00 E5 C0 00 E5 BE 82 3A 1A 50 54 2C 68 08 AB 7E E0 00 E4 DC"
-        "00 E2 D9 00 E5 D5 00 E3 D3 00 E4 D0 00 E9 CD 00 E4 CA 00 E2 C7 00 E9 C5 00 E5"
-        "C2 00 E5 C0 00 E5 BE 82 3A 1A 50 54 2C 68 08 AB 7E E0 00 E4 DC 00 E2 D9 00 E5"
-        "D5 00 E3 D3 00 E4 D0 00 E9 CD 00 E4 CA 00 E2 C7 00 E9 C5 00 E5 C2 00 E5 C0 00"
-        "E5 BE 82 3A 1A 50 54 2C 68 08 AB 7E E0 00 E4 DC 00 E2 D9 00 E5 D5 00 E3 D3 00"
-        "E4 D0 00 E9 CD 00 E4 CA 00 E2 C7 00 E9 C5 00 E5 C2 00 E5 C0 00 E5 BE 82 3A 1A"
-        "50 54 2C 68 08 AB 7E E0 00 E4 DC 00 E2 D9 00 E5 D5 00 E3 D3 00 E4 D0 00 E9 CD"
-        "00 E4 CA 00 E2 C7 00 E9 C5 00 E5 C2 00 E5 C0 00 E5 BE 82 3A 1A 50 54 2C 68 08"
-        "AB 7E E0 00 E4 DC 00 E2 D9 00 E5 D5 00 E3 D3 00 E4 D0 00 E9 CD 00 E4 CA 00 E2"
-        "C7 00 E9 C5 00 E5 C2 00 E5 C0 00 E5 BE 82 3A 1A 50 54 2C 68 08 AB 7E E0 00 E4"
-        "DC 00 E2 D9 00 E5 D5 00 E3 D3 00 E4 D0 00 E9 CD 00 E4 CA 00 E2 C7 00 E9 C5 00"
-        "E5 C2 00 E5 C0 00 E5 BE 82 3A 1A 50"
+        "E4 CA 00 E2 C7 00 E9 C5 00 E5 C2 00 E5 C0 00 E5 BE 82 3A 1A 50"
     )
 )
 
@@ -49,62 +36,50 @@ def chunk(byte_input: bytes) -> int:
     return int.from_bytes(byte_input, "big")
 
 
-def read_data(raw_bytes: bytes) -> List[Reading]:
-    """Convert data packet into a list of Readings"""
+def read_data(each_bytes: bytes) -> Reading:
+    """Convert data packet into a Reading"""
 
-    bytes_split = raw_bytes.split(b"T")
+    if len(each_bytes) != 47:  # raise on incomplete reads
+        raise ValueError("Incomplete data packet.")
 
-    readings_list: List[Reading] = []
-    for each_bytes in bytes_split:
-        # print(each.hex())
-        if len(each_bytes) != 46:  # Ignore incomplete reads
-            continue
+    # Analyze each complete reading
+    radar_speed = chunk(each_bytes[3:4] + each_bytes[2:3])
+    start_angle = chunk(each_bytes[5:6] + each_bytes[4:5])
+    data = each_bytes[6:42]
+    end_angle = chunk(each_bytes[43:44] + each_bytes[42:43])
+    timestamp = chunk(each_bytes[45:46] + each_bytes[44:45])
+    # crc_check = chunk(input[46:47])
 
-        # Analyze each complete reading
-        each_bytes = b"T" + each_bytes  # Put back the start character that was stripped
+    data_list = [data[i : i + 3] for i in range(0, len(data), 3)]
 
-        radar_speed = chunk(each_bytes[3:4] + each_bytes[2:3])
-        start_angle = chunk(each_bytes[5:6] + each_bytes[4:5])
-        data = each_bytes[6:42]
-        end_angle = chunk(each_bytes[43:44] + each_bytes[42:43])
-        timestamp = chunk(each_bytes[45:46] + each_bytes[44:45])
-        # crc_check = chunk(input[46:47])
+    if end_angle > start_angle:  # Calculate Angle Increment
+        angle_inc = (end_angle - start_angle) / 11
+    else:
+        angle_inc = (end_angle + 36000 - start_angle) / 11
 
-        data_list = [data[i : i + 3] for i in range(0, len(data), 3)]
+    angles = [start_angle + i * angle_inc for i in range(0, 12)]
 
-        if end_angle > start_angle:  # Calculate Angle Increment
-            angle_inc = (end_angle - start_angle) / 11
-        else:
-            angle_inc = (end_angle + 36000 - start_angle) / 11
-
-        angles = [start_angle + i * angle_inc for i in range(0, 12)]
-
-        out_data = [
-            Point(
-                distance=chunk(each[1:2] + each[0:1]),
-                angle=angle,
-                confidence=chunk(each[2:3]),
-            )
-            for (each, angle) in zip(data_list, angles)
-        ]
-
-        readings_list.append(
-            Reading(
-                radar_speed=radar_speed,
-                start_angle=start_angle,
-                end_angle=end_angle,
-                timestamp=timestamp,
-                points=out_data,
-            )
+    out_data = [
+        Point(
+            distance=chunk(each[1:2] + each[0:1]),
+            angle=angle,
+            confidence=chunk(each[2:3]),
         )
+        for (each, angle) in zip(data_list, angles)
+    ]
 
-    return readings_list
+    return Reading(
+        radar_speed=radar_speed,
+        start_angle=start_angle,
+        end_angle=end_angle,
+        timestamp=timestamp,
+        points=out_data,
+    )
 
 
 if __name__ == "__main__":
-    print(read_data(SAMPLE)[0])
 
-    assert str(read_data(SAMPLE)[0]) == (
+    assert str(read_data(SAMPLE)) == (
         "Reading(radar_speed=2152, start_angle=32427, end_angle=33470, "
         "timestamp=6714, points=[Point(distance=224, angle=32427.0, confidence=228), "
         "Point(distance=220, angle=32521.81818181818, confidence=226), "
@@ -119,3 +94,5 @@ if __name__ == "__main__":
         "Point(distance=194, angle=33375.181818181816, confidence=229), "
         "Point(distance=192, angle=33470.0, confidence=229)])"
     ), "Reading is not identical"
+
+    print("Read sample data correctly.")
